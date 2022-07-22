f6e0e9d02811ec9f2e92b1c6016ed6cdf655b221
Dimitar Dimitrov <deobox@gmail.com>
2022-07-22 15:56:04 +0100

    Mixed Updates

--- mixed.sh
+++ mixed.sh
@@ -3,19 +3,32 @@ alias winkey='hexdump -s 56 -e '\''"Windows BIOS key: " /29 "%s\n"'\'' /sys/firm
 alias ifconfig='ip -co -st -h a';
 
 unshare --mount --uts --ipc --net --pid --fork --user --map-root-user /bin/bash;
+unshare -miupfrR /home/user/rootfs --mount-proc;
 
 brctl addbr br0; ip addr add 10.0.0.2/24 dev br0; ip link set dev br0 up; brctl addif eth0; ip tuntap add vn0 mode tap; brctl addif br0 vn0;
 
 nmap -sn 10.0.0.0/24; nmap -T4 -v -A -sV -sS -Pn -O -sC 127.0.0.1;
 
+wget 'https://yt-dl.org/downloads/latest/youtube-dl' -O 'youtube-dl' && chmod 0700 'youtube-dl' && sed -i '1 s|env python|env python3|g' 'youtube-dl';
+youtube-dl {-F -f 251} "file.mp4"; ffprobe "file.mp4";
+ffmpeg -i file.mp4 -b:a 320K -vn file.mp3; ffmpeg -i file.flac -ab 320k file.mp3;
+for file in *.webm; do ffmpeg -i "${file}" -b:a 160K -vn "${file%.*}".mp3; done;
+ffmpeg -i "concat:file1.mp3|file2.mp3" -acodec copy output.mp3
+
+ffmpeg -f x11grab -s 1600x900 -i :0.0 -framerate 25 -preset ultrafast -f alsa -ac 2 -i hw:0 out.mp4;
+
 apt install dnsmasq; nmcli dev wifi hotspot ifname wless0 ssid "WiFiName" password "WiFiPass";
 nmcli r wifi on && nmcli d wifi connect "WiFiName" password "WiFiPass";
+nmcli d w c AA:BB:CC:DD:EE:FF password "WiFiPass";
 
 certbot certonly --manual --register-unsafely-without-email -d mydomain.com;
 
 dd if=/dev/sda of=file.img bs=$(($(blockdev --getbsz /dev/sda)*2048)) conv=sync,noerror status=progress oflag=direct;
 
 curl --silent "http://ipinfo.io/8.8.8.8" |  python3 -c "import sys, json; print(json.load(sys.stdin)['region'])";
+
+socat TCP4-LISTEN:8080,fork,su=nobody TCP6:[IPv6:Adress]:80;
+6tunnel -f -4 8080 IPv6:Adress 80;
 
 sed -rne '/10\/Oct\/2020/,/09\/Nov\/2020/ p' file.log;
 
@@ -37,11 +50,15 @@ tar --selinux --acls --xattrs -czvf file.tgz file-dir;
 semanage port -a -t ssh_port_t -p tcp 11; semanage port -l | grep ssh; systemctl restart sshd.service;
 semanage fcontext -a -t samba_share_t /etc/file1; restorecon -R -v /etc/file1; semanage fcontext -d /test;
 
-ffmpeg -i file.mp4 -b:a 320K -vn file.mp3;
 apt install alsa-utils; amixer set Master unmute; amixer sset 'Master' 60%; amixer -q sset Master 3%+;
 
 gio mount -li; gio mount mtp://[usb:XXX,YYY];
 apt install jmtpfs; mkdir -p mtpdevice; chown $USER:$USER mtpdevice; jmtpfs mtpdevice; fusermount -u mtpdevice;
+
+udevadm info -a -n /dev/sdx | grep serial; udevadm monitor -kup |& tee /tmp/udev.log;
+echo 'ACTION=="add", SUBSYSTEM=="block", ATTRS{serial}=="1122334455", RUN+="/usbplugged.sh" ' >> /etc/udev/rules.d/99-usb-plugged.rules; udevadm control --reload; tail -n 0 -f /var/log/syslog;
+
+x11vnc -storepasswd; x11vnc -forever -loop -alwaysshared -nolookup -localhost -listen 127.0.0.1 -no6 -noipv6 -nobell -noxdamage -noscr -nowf -cursor arrow -xkb -display :0 -rfbauth ~/.vnc/passwd >> ~/vnc.log 2>&1 &
 
 apt install lxde-core pulseaudio pavucontrol lxterminal dbus dbus-x11 dbus-user-session xserver-xorg-core xserver-xorg-core xserver-xorg-video-all xserver-xorg-input-all x11-xserver-utils x11-xkb-utils x11-utils xinit;
 apt install lxqt-core lxterminal pulseaudio pavucontrol-qt dbus dbus-x11 dbus-user-session xserver-xorg-core adwaita-icon-theme gnome-icon-theme-nuovo openbox xserver-xorg-core xserver-xorg-video-all xserver-xorg-input-all x11-xserver-utils x11-xkb-utils x11-utils xinit;
